0e400755fbda2a30e404030cf4ecbbe380d23c48
HoaNguyen1903 <hoanhtse185071@fpt.edu.vn>
2025-03-25 13:38:35 +0700

    Update Sidebar.jsx
    Removed not needed comments

--- GymFaceID_FE/src/components/Sidebar.jsx
+++ GymFaceID_FE/src/components/Sidebar.jsx
@@ -7,16 +7,15 @@ import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 import BusinessIcon from '@mui/icons-material/Business';
 import { useNavigate } from "react-router-dom";
 import { useSwipeable } from 'react-swipeable';
-
 import Logo from '../assets/LightMainLogo.png'
 import './styles/Sidebar.css'
 import LogoutButton from "./Account-Actions/LogoutButton";
+
 const { Sider } = Layout
 
 const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false);
     const navigate = useNavigate();
-
     const handlers = useSwipeable({
         onSwipedLeft: () => setCollapsed(true),
         onSwipedRight: () => setCollapsed(false),
@@ -24,17 +23,32 @@ const Sidebar = () => {
         trackMouse: false,
     });
 
+    const handleLogoClick = () => {
+        navigate("/");
+    };
+
     return (
-        <Sider collapsible
+        <Sider
+            collapsible
             collapsed={collapsed}
             onCollapse={setCollapsed}
             trigger={null}
-            className="mainLaySidebar" style={{ minHeight: "100vh" }}>
-            <div className="logo">
+            className="mainLaySidebar"
+            style={{ minHeight: "100vh" }}
+        >
+            <div
+                className="logo"
+                onClick={handleLogoClick}
+                style={{ cursor: 'pointer' }}
+            >
                 {collapsed ? "Gym" : <img src={Logo} alt="Logo" />}
             </div>
-
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]} onClick={(e) => navigate(e.key)}>
+            <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={["1"]}
+                onClick={(e) => navigate(e.key)}
+            >
                 <Menu.Item key="/AdminPage/UserManagement" icon={<PersonIcon style={{ fontSize: "24px" }} />}>
                     User Management
                 </Menu.Item>
@@ -47,20 +61,7 @@ const Sidebar = () => {
                 <Menu.Item key="/AdminPage/CustomerMembershipManagement" icon={<BusinessIcon style={{ fontSize: "24px" }} />}>
                     Customer Membership Managment
                 </Menu.Item>
-                {/*<Menu.Item key="/" icon={<DriveFileRenameOutline style={{ fontSize: "24px" }}/>}>*/}
-                {/*  Func 2*/}
-                {/*</Menu.Item>*/}
-                {/*<Menu.Item key="/" icon={<UserOutlined style={{ fontSize: "24px" }}/>}>*/}
-                {/*  Func 3*/}
-                {/*</Menu.Item>*/}
-                {/*<Menu.Item key="/" icon={<CalendarMonthOutlined style={{ fontSize: "24px" }}/>}>*/}
-                {/*  Func 4*/}
-                {/*</Menu.Item>*/}
-                {/*<Menu.Item key="/" icon={<SmsOutlined style={{ fontSize: "24px" }}/>}>*/}
-                {/*  Func 5*/}
-                {/*</Menu.Item>*/}
             </Menu>
-
             <LogoutButton />
             <div
                 className="collapseBtn"
